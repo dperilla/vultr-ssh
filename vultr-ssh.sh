a4194fb5f7640600c1a9a83849ec54d042a14533
David <dperilla@gmail.com>
2014-12-07 23:02:44 +0100

    Update vultr-ssh.sh

--- vultr-ssh.sh
+++ vultr-ssh.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Require vultr-cli https://github.com/yikaus/vultrcli
-portssh="22" # Port ssh 
-apikey="" # Api key vultr
+port_ssh="22" # Port ssh 
+api_key="" # Api key vultr
 clear
-/usr/local/bin/vucli.py serverlist -k $apikey|awk '{print $2}'|grep -v label|sort > /tmp/vpsvultr
+/usr/local/bin/vucli.py serverlist -k $api_key|awk '{print $2}'|grep -v label|sort > /tmp/vpsvultr
 cat -b /tmp/vpsvultr
 echo ""
 printf '%s ' 'vps number?'
 read -${BASH_VERSION+e}r vps
 vps2=$(sed -n $vps"p" /tmp/vpsvultr|awk '{print $1}')
 echo $vps2
-ssh root@$vps2 -p $portssh
+ssh root@$vps2 -p $port_ssh
 exit 0
